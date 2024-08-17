7af520b0cb7d30d445812b9dc95d46a86c3f11c4
ShahidBinShafiq <shahidvital786@gmail.com>
2024-08-18 02:30:47 +0500

    Working on footer section almost completed

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ window.addEventListener("scroll", function(){
 
 // styling of testimonial-slider section
 
-var swiper = new Swiper("#mySwiper", {
+var myswiper = new Swiper("#myswiper", {
     
     navigation: {
       nextEl: ".swiper-button-next",
@@ -28,15 +28,81 @@ var swiper = new Swiper("#mySwiper", {
   });
 
 
-  // styling of portfolio swiper 
+ 
 
 
-  var swiperA = new Swiper("#swiperA", {
-    slidesPerView: 5,
-    spaceBetween: 15,
+
+const swiperA = new Swiper("#swiperA", {
+    slidesPerView: 1,
+    // spaceBetween: 60,
     loop: "true",
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
+
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      992: {
+        slidesPerView: 5,
+        spaceBetween: 40
+      }
+    }
   });
+
+  //styling of prod_sect section
+
+  let prod_sect = document.querySelector("#prod_sect");
+  
+  let podSectHalfHeight = prod_sect.offsetHeight / 2;
+
+  let portfolioSect = document.querySelector("#portfolioSect");
+
+  portfolioSect.style.marginBottom = podSectHalfHeight + "px";
+
+  let newsSect = document.querySelector("#newsSect");
+
+  newsSect.style.paddingTop = podSectHalfHeight + "px";
+
+  // let header_height = header_sect.offsetHeight;
+
+//   let dateInput = document.querySelectorAll("#dateInput")
+
+//   flatpickr(dateInput, {
+//     dateFormat: "Y-m-d", // Adjust the format as needed
+// });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  let dateInput = document.getElementById("dateInput");
+
+  // Event listener to focus on the input to trigger the date picker
+  dateInput.addEventListener("click", function() {
+      dateInput.focus(); // Programmatically focus the input to show the date picker
+  });
+});
+
+
+
+ //styling of video_sect
+
+ let faqsSect = document.querySelector("#faqsSect");
+
+ let videoSect = document.querySelector("#videoSect");
+  
+ let videoSectHeight = vidHeight.offsetHeight / 2;
+
+
+ faqsSect.style.marginBottom = videoSectHeight + "px";
+
+//  let newsSect = document.querySelector("#newsSect");
+
+videoSect.style.paddingTop = videoSectHeight + "px";
+
